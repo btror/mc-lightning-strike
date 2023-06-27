edd4e2186e46a59b31d4fd970606f777f5d7d547
Brandon <ticer1999@gmail.com>
2023-06-26 20:20:11 -0400

    create basic cloud and add sound effects

--- src/main/java/mclightningstrike/mclightningstrike/lightning/pathfinding/Animation.java
+++ src/main/java/mclightningstrike/mclightningstrike/lightning/pathfinding/Animation.java
@@ -1,10 +1,10 @@
 package mclightningstrike.mclightningstrike.lightning.pathfinding;
 
 import mclightningstrike.mclightningstrike.McLightningStrike;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.Particle;
-import org.bukkit.World;
+import org.bukkit.*;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import static org.apache.logging.log4j.LogManager.getLogger;
 
@@ -36,20 +36,47 @@ public class Animation {
     public void start() {
         createLightningStrikeZoneSimulation();
         startLightningStrikeSimulation();
-        if (completedSimulationLightningZone != null) {
-            for (int i = 0; i < completedSimulationLightningZone.length; i++) {
-                for (int j = 0; j < completedSimulationLightningZone[0].length; j++) {
-                    for (int k = 0; k < completedSimulationLightningZone[0][0].length; k++) {
-                        if (completedSimulationLightningZone[i][j][k] == 3) {
-                            // lightningZone[i][j][k].getBlock().setType(Material.DIAMOND_BLOCK);
-                            World w = lightningZone[i][j][k].getBlock().getLocation().getWorld();
-                            w.spawnParticle(
-                                    Particle.HEART, // NOTE
-                                    lightningZone[i][j][k].getBlock().getLocation(),
-                                    1
-                            );
+        // strikeStart.getWorld().spawnEntity(strikeStart, EntityType.AREA_EFFECT_CLOUD);
+        createCloud();
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                strikeStart.getWorld().playSound(strikeStart, Sound.ENTITY_LIGHTNING_BOLT_THUNDER, 100,30);
+                if (completedSimulationLightningZone != null) {
+                    for (int i = 0; i < completedSimulationLightningZone.length; i++) {
+                        for (int j = 0; j < completedSimulationLightningZone[0].length; j++) {
+                            for (int k = 0; k < completedSimulationLightningZone[0][0].length; k++) {
+                                if (completedSimulationLightningZone[i][j][k] == 3) {
+                                    World w = lightningZone[i][j][k].getBlock().getLocation().getWorld();
+                                    w.spawnParticle(
+                                            Particle.HEART,
+                                            lightningZone[i][j][k].getBlock().getLocation(),
+                                            1
+                                    );
+                                }
+                            }
                         }
                     }
+                }
+                cancel();
+            }
+        }.runTaskTimer(plugin, 25L, 20L);
+
+    }
+
+    public void createCloud() {
+        for (int i = 1; i < 6; i++) {
+            for (int j = 1; j < 6; j++) {
+                int randomY = (int)(Math.random() * (3)) + 1;
+                int randomGen = (int)(Math.random() * 6);
+                if (randomGen % 3 == 0) {
+                    Location location = new Location(
+                            strikeStart.getWorld(),
+                            strikeStart.getBlock().getX() - 5 + (i * 2),
+                            strikeStart.getBlock().getY() + randomY,
+                            strikeStart.getBlock().getZ() - 5 + (j * 2)
+                    );
+                    strikeStart.getWorld().spawnEntity(location, EntityType.AREA_EFFECT_CLOUD);
                 }
             }
         }
