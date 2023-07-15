1adba990482e77e54c25952ae3bf8f40b0681084
Brandon <ticer1999@gmail.com>
2023-07-15 17:01:48 -0400

    change from arrow to potion of harming

--- src/main/java/mclightningstrike/mclightningstrike/storm/StormListener.java
+++ src/main/java/mclightningstrike/mclightningstrike/storm/StormListener.java
@@ -1,51 +1,56 @@
 package mclightningstrike.mclightningstrike.storm;
 
 import mclightningstrike.mclightningstrike.McLightningStrike;
-import org.bukkit.Material;
-import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.ThrownPotion;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.event.entity.ProjectileLaunchEvent;
+import org.bukkit.potion.PotionEffect;
 
 import java.util.*;
 
-import static org.apache.logging.log4j.LogManager.getLogger;
-
 public record StormListener(McLightningStrike plugin) implements Listener {
 
-    private static final Map<UUID, Material> arrows = new HashMap<>();
+    private static final Map<UUID, PotionEffect> projectiles = new HashMap<>();
 
     /**
-     * Event handler for shooting an arrow.
+     * Event handler for shooting a projectile.
      *
      * @param event ProjectileLaunchEvent
      */
     @EventHandler
     public void onProjectileShot(ProjectileLaunchEvent event) {
-        if (event.getEntity() instanceof Arrow) {
+        if (event.getEntity() instanceof ThrownPotion) {
             Player shooter = (Player) event.getEntity().getShooter();
             assert shooter != null;
-            getLogger().info("item: " + shooter.getInventory().getItemInMainHand().getType());
-            StormListener.arrows.put(event.getEntity().getUniqueId(), shooter.getInventory().getItemInMainHand().getType());
+            Collection<PotionEffect> effects = ((ThrownPotion) event.getEntity()).getEffects();
+            for (PotionEffect effect : effects) {
+                if (effect.getType().getName().equalsIgnoreCase("HARM")) {
+                    projectiles.put(event.getEntity().getUniqueId(), effect);
+                    break;
+                }
+            }
         }
     }
 
     /**
-     * Event handler for the arrow on landing.
+     * Event handler for the harm potion on landing.
      *
      * @param event ProjectileHitEvent
      */
     @EventHandler
     public void onProjectileHit(ProjectileHitEvent event) {
-        if (event.getEntity() instanceof Arrow && arrows.containsKey(event.getEntity().getUniqueId())) {
-            if (arrows.get(event.getEntity().getUniqueId()) == Material.BOW) {
+        if (event.getEntity() instanceof ThrownPotion && projectiles.containsKey(event.getEntity().getUniqueId())) {
+            if (projectiles.get(event.getEntity().getUniqueId()).getType().getName().equalsIgnoreCase("HARM")
+                    && projectiles.get(event.getEntity().getUniqueId()).getAmplifier() == 0) {
                 new Storm(plugin, event.getEntity().getLocation()).generate(false);
-            } else if (arrows.get(event.getEntity().getUniqueId()) == Material.CROSSBOW) {
+            } else if (projectiles.get(event.getEntity().getUniqueId()).getType().getName().equalsIgnoreCase("HARM")
+                    && projectiles.get(event.getEntity().getUniqueId()).getAmplifier() == 1) {
                 new Storm(plugin, event.getEntity().getLocation()).generate(true);
             }
-            arrows.remove(event.getEntity().getUniqueId());
+            projectiles.remove(event.getEntity().getUniqueId());
         }
     }
 }
